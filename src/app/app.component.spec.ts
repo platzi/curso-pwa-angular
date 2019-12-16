b94721ab47e36dc59fea92d2a7d97437d68791c7
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-12-16 13:49:57 -0500

    :construction: work

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'platzi-store-pwa'`, () => {
+  it(`should have as title 'platzi-store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('platzi-store-pwa');
+    expect(app.title).toEqual('platzi-store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('platzi-store-pwa app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('platzi-store app is running!');
   });
 });
