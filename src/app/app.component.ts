aa4ae20bdd2fc96d9126e2dcdf9de898a0d5b96c
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-12-17 17:31:07 -0500

    :construction: work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  
+  constructor(
+    private swUpdate: SwUpdate,
+  ) {}
+
+  ngOnInit() {
+    this.updatePWA();
+  }
+
+  updatePWA() {
+    this.swUpdate.available
+    .subscribe(value => {
+      console.log('update:', value);
+      window.location.reload();
+    });
+  }
 }
