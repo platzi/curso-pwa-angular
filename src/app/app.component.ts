12939710a243e1ff78ed9abdad3b68fdbc6725c6
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-12-17 17:05:54 -0500

    :construction: deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 import { map } from 'rxjs/operators';
 
@@ -13,6 +13,7 @@ import { Observable } from 'rxjs';
 export class HeaderComponent implements OnInit {
 
   total$: Observable<number>;
+  installEvent = null;
 
   constructor(
     private cartService: CartService
@@ -24,6 +25,23 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: Event) {
+    console.log(event);
+    event.preventDefault();
+    this.installEvent = event;
+  }
+
+  installByUser() {
+    if (this.installEvent) {
+      this.installEvent.prompt();
+      this.installEvent.userChoice
+      .then(rta => {
+        console.log(rta);
+      });
+    }
   }
 
 }
