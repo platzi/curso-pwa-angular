e767659a58959dd3e603ccc1ec82193376ec0550
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2019-12-18 14:26:34 -0500

    :pushpin: ppush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { SwUpdate, SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,13 @@ export class AppComponent implements OnInit {
 
   constructor(
     private swUpdate: SwUpdate,
+    private swPush: SwPush
   ) {}
 
   ngOnInit() {
     this.updatePWA();
+    this.listenNotifications();
+    this.register();
   }
 
   updatePWA() {
@@ -21,6 +24,23 @@ export class AppComponent implements OnInit {
     .subscribe(value => {
       console.log('update:', value);
       window.location.reload();
+    });
+  }
+
+  listenNotifications() {
+    this.swPush.messages
+    .subscribe(msg => {
+      console.log(msg);
+    });
+  }
+
+  register() {
+    const key = 'BLDV1q-_njNKmAfA5SZYxnaicR1l1nzxYcd9VH4qJdFO4tm0TmwOF7uno67Gku3Eoygko8TF6kOdW3FwH62FIs8';
+    this.swPush.requestSubscription({
+      serverPublicKey: key
+    })
+    .then(data => {
+      console.log(data.toJSON());
     });
   }
 }
